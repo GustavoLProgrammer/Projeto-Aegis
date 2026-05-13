d7099cbb3eaf2f883afca757a810ec2d50b84015
Gustavo Lopes <gu45363@gmail.com>
2026-05-12 22:41:46 -0300

    Concluído: Interface interativa WearOS e Processador Python com log

--- core_processamento/processador_denuncia.py
+++ core_processamento/processador_denuncia.py
@@ -1,10 +1,17 @@
 # Armazenamento temporário
+
+from datetime import datetime
+
 historico_denuncias = []
 
 def registrar_denuncia(tipo_solicitacao):
     """
     Funçâo para receber 'Emergência' ou 'Ajuda'
     """
+    agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    with open("log_denuncias.txt", "a") as arquivo:
+        arquivo.write(f"[{agora}] Alerta: {tipo_solicitacao}\n")
+        
     # Adiciona o tipo a lista
     historico_denuncias.append(tipo_solicitacao)
     print(f"Sucesso: {tipo_solicitacao} registrado no sistema.")
--- interface_wearos/ReportFragment.kt
+++ interface_wearos/ReportFragment.kt
@@ -1,11 +1,26 @@
 // classe ReportFragment
+import android.widget.TextView
+import android.widget.Button
+import android.graphics.Color 
+
 override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    val tvTitulo = view.findViewById<TextView>(R.id.tvTitulo)
 
     //Conectando o código ao botão do XML
     val btnEmergencia = view.findViewById<(R.id.btnEmergencia)
 
     btnEmergencia.setOnClickListener {
         println("Alerta: botão de Emergência acionado!")
+    }
+    
+    // Simula o início do sistema de monitoramento/quiz
+    val btnAjuda = view.findViewById<Button>(R.id.btnAjuda)
+
+    btnAjuda.setOnClickListener {
+        tvTitulo.text = "Você está em perigo agora?"
+        btnEmergencia.text = "SIM"
+        btnAjuda.text = "NÃO"
+        println("Quiz de monitoramento iniciado!")
     }
 }
